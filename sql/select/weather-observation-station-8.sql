d30341ef15d28c2178977e474a9d037f8f73d0a2
Seong Yong-ju <sei40kr@gmail.com>
2017-12-12 00:32:03 +0900

    Add weather-observation-station-8

---
+++ sql/select/weather-observation-station-8.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (CITY LIKE 'a%'
+       OR CITY LIKE 'i%'
+       OR CITY LIKE 'u%'
+       OR CITY LIKE 'e%'
+       OR CITY LIKE 'o%')
+  AND (CITY LIKE '%a'
+       OR CITY LIKE '%i'
+       OR CITY LIKE '%u'
+       OR CITY LIKE '%e'
+       OR CITY LIKE '%o')
